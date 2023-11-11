7d57d8434fd3db5425c619813f8d3a94a231f696
AmeliaLis <lis.amelia.maria@gmail.com>
2023-11-11 18:16:37 +0100

    base rate driver

--- Modules/Driver.py
+++ Modules/Driver.py
@@ -6,21 +6,22 @@ class Driver:
         self.name = name
         self.surname = surname
         self.hireDate = None
-        self.hourlyRate = None
+        self.hourlyBaseRate = None
     
 
     def setHireDate(self, year, month, day):
         try:
-            if (year > date.today().year):
+            if (date(year,month,day) > date.today()):
                 print("Hire date cannot be set in the future.")
             else:
                 self.hireDate = date(year=year, month=month, day=day)
-                print(self.hireDate.strftime("%x"))
+                print("Hire date has been set to: " + self.hireDate.strftime("%x") + ".")
         except ValueError as e:
             print(f"An error has occured: {e}")
 
-    def setHourlyRate(self, rate):
+    def setHourlyBaseRate(self, rate):
         if (self.hireDate != None):
-            self.hourlyRate = rate
-
-driver = Driver("Jan", "Kowalski")
+            self.hourlyBaseRate = rate
+            print("Horly rate has been set to: " + str(self.hourlyBaseRate) +" PLN.")
+        else:
+            print("Firstly set hire date.")
