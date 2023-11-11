4df4679cd3b2306b4b74d382d9f1859ae61f9762
szymonkonopek <sk.gornik@gmail.com>
2023-11-11 12:48:11 +0100

    update

--- Modules/Gpt.py
+++ Modules/Gpt.py
@@ -1,13 +1,15 @@
 import os
-import openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key="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")
+
 import json
 
 class Gpt:
     @staticmethod
     def calcDistance(cityA, cityB):
-        completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        completion = client.chat.completions.create(model="gpt-3.5-turbo",
         messages = [
             {"role": "system", "content": "Generate road length in kilometers between 2 cities A and B in json file. only variable=length"},
             {"role": "user", "content": f"A={cityA}, B={cityB}"}
--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - install VSCode extension `https://marketplace.visualstudio.com/items?itemName=tamasfe.even-better-toml`
 
 1. `poetry install` to install dependencies
+2. `poetry self add poetry-dotenv-plugin`
+
 <!-- 2. `poetry shell` -> `poe start` or `poetry run poe start` to start flask server
 2. to exit shell `exit`
 3. to run individual script `poetry shell` then use vscode code runner -->
