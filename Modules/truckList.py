ac6ab34b30a949b5c6d891f811e212ed8893c61f
szymonkonopek <sk.gornik@gmail.com>
2023-11-20 19:45:40 +0100

    truckList

--- Modules/truckList.py
+++ Modules/truckList.py
@@ -1,6 +1,6 @@
 import json
 import uuid
-
+from Modules.Truck import Truck
 
 class TruckList:
     def __init__(self):
@@ -8,7 +8,7 @@ class TruckList:
             self.truckList = json.load(f)
 
     def addTruck(self, truck):
-        self.truckList["trucks"].append(self.truckToJson(truck))
+        self.truckList.append(self.truckToJson(truck))
         print('trucklist', self.truckList)
         with open('truckList.json', 'w') as f:
             json.dump(self.truckList, f, indent=4)
@@ -22,6 +22,9 @@ class TruckList:
             "fuelEconomy": truck.fuelEconomy,
             "otherThings": truck.otherThings
         }
+    
+    def jsonToTruck(self, truckJson):
+        return Truck(truckJson['brand'], truckJson['model'], int(truckJson['capacity']), int(truckJson['fuelEconomy']), truckJson['otherThings'])
 
     def deleteTruck(self, truck_id):
         self.truckList = [truck for truck in self.truckList if truck['id'] != truck_id]
@@ -35,7 +38,11 @@ class TruckList:
         return None
     
     def getTruckList(self):
-        return self.truckList["trucks"]
+        for i in range(len(self.truckList)):
+            self.truckList[i] = self.jsonToTruck(self.truckList[i])
+        print('hello')
+        print(getattr(self.truckList[0], 'fuelEconomy'))
+        return self.truckList
     
     def updateTruck(self, truck_id, truck):
         for i in range(len(self.truckList)):
